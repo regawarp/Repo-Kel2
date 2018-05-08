d653a599eb5af5c2fc7e4d3e8c513fe44c16b82c
astro-rizki <srizkiafrizal85@gmail.com>
2018-05-08 11:17:06 +0700

    merging dashboard

---
+++ FoodCourt/src/foodcourt/data.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright May - 2018
+ * JTK POLBAN.
+ * Made in Ciwaruga.
+ */
+package foodcourt;
+
+/**
+ *
+ * @author ASUS
+ */
+public class data {
+    private String title;
+    private int info;
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the info
+     */
+    public int getInfo() {
+        return info;
+    }
+
+    /**
+     * @param info the info to set
+     */
+    public void setInfo(int info) {
+        this.info = info;
+    }
+    
+    
+    public void readFromExcel(String id){
+        /*Do Nothing*/
+    }
+       
+}
