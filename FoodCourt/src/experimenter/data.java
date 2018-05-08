73dc9920ae053ecb380f8eaedaaf68ef73e70c81
astro-rizki <srizkiafrizal85@gmail.com>
2018-05-08 09:06:59 +0700

    menggabung dashboard

---
+++ FoodCourt/src/experimenter/data.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright May - 2018
+ * JTK POLBAN.
+ * Made in Ciwaruga.
+ */
+package experimenter;
+
+/**
+ *
+ * @author ASUS
+ */
+public class data {
+    
+}
---
+++ FoodCourt/src/swing/dataController.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright May - 2018
+ * JTK POLBAN.
+ * Made in Ciwaruga.
+ */
+package swing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ASUS
+ */
+public class dataController {
+//    List<data> model = new ArrayList<>();
+    
+}
